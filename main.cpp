61bb7beaae0d55c2af79f51fe9f640af6156f12f
hnhat19 <89629758+hnhat19@users.noreply.github.com>
2026-04-10 23:38:48 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,37 +1,12 @@
 #include <iostream>
-#include "PhanSo.h"
+#include "DATE.h"
 using namespace std;
 
-int main() {
-    PhanSo a, b;
-    cout << "Nhap phan so A:\n";
+int main(){
+    DATE a;
     a.Nhap();
-    cout << "Nhap phan so B:\n";
-    b.Nhap();
-    cout << "\nPhan so A: ";
-    a.RutGon();
     a.Xuat();
-    cout << "\nPhan so B: ";
-    b.RutGon();
-    b.Xuat();
-    // Tính toán
-    PhanSo tong = a.Tong(b);
-    PhanSo hieu = a.Hieu(b);
-    PhanSo tich = a.Tich(b);
-    PhanSo thuong = a.Thuong(b);
-
-    cout << "\n\nTong: ";
-    tong.Xuat();
-    cout << "\nHieu: ";
-    hieu.Xuat();
-    cout << "\nTich: ";
-    tich.Xuat();
-    cout << "\nThuong: ";
-    thuong.Xuat();
-
-    // So sánh
-    cout<<endl<<"Phan so A ";a.SoSanh(b);cout<<" Phan so B"<<endl;
-
+    a.NgayThangNamTiepTheo();
     return 0;
 }
 
