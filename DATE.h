b9e451e348e8daca1f8474c33ee2a1e736ba6289
hnhat19 <89629758+hnhat19@users.noreply.github.com>
2026-04-10 23:28:51 +0700

    Add files via upload

---
+++ DATE.h
@@ -0,0 +1,17 @@
+#ifndef DATE_H
+#define DATE_H
+
+class DATE {
+private:
+    int iNgay;
+    int iThang;
+    int iNam;
+public:
+    void Nhap();
+    void Xuat();
+    void NgayThangNamTiepTheo();
+
+};
+
+#endif
+
